1c530e20ca45558da23a962984f2ffc76bdf27d9
Bayron_Perez <boperez3@misena.edu.co>
2023-03-24 23:59:27 -0500

    Actualizado 24
    Actualizado 24

--- tareas/urls.py
+++ tareas/urls.py
@@ -14,8 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from todo import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home,name='home'),
+    path('agregar/', views.agregar, name='agregar'),
+    path("eliminar/<int:tarea_id>/",views.eliminar,name="eliminar"),
+    path("editar/<int:tarea_id>/",views.editar,name="editar"),
+    #path("", include("todo.urls")),
+    
 ]
